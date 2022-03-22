7ca3d9f0a04e26a71f580901fe0927c7acfa5bd5
matthewgraca <mgraca@cpp.edu>
2022-03-21 21:36:26 -0700

    implementation of rr added

--- src/main/java/com/mgraca/app/CpuSchedulingAlgorithmsApp.java
+++ src/main/java/com/mgraca/app/CpuSchedulingAlgorithmsApp.java
@@ -109,17 +109,19 @@ public class CpuSchedulingAlgorithmsApp{
    * Runs the processes on a list as they are; currently only fit for fcfs and sjf
    * @param processList the list of processes
    */
-  private static void runProcesses(ArrayList<int[]> processList){
+  private static void runContiguousProcesses(ArrayList<int[]> processList){
     int n = processList.size();
+    int timer = 0;
 
     for (int i = 0; i < n; i++){
       int total = processList.get(i)[burst];
       int progress = 0;
       // display progress
-      while (progress <= total){
+      while (progress < total){
         try{
-          System.out.print("\rProcess " + processList.get(i)[pid] + " progress: " + progress + "/" + total);
           progress++;
+          timer++;
+          System.out.print("\rProcess " + processList.get(i)[pid] + " progress: " + progress + "/" + total);
           Thread.sleep(100); // delay to see progress
         }
         catch (InterruptedException e){
@@ -130,11 +132,12 @@ public class CpuSchedulingAlgorithmsApp{
     }
 
     // timings for processes that are run contiguously
-    int totalTime = 0;
+    int totalBurstTime = 0;
     int totalWaitTime = 0;
     int totalTurnaroundTime = 0;
     int sumOfPrevTimes = 0;
     for (int i = 0; i < n; i++){
+      totalBurstTime += processList.get(i)[burst];
       totalTurnaroundTime += sumOfPrevTimes - processList.get(i)[arrive] + processList.get(i)[burst];
       totalWaitTime += sumOfPrevTimes - processList.get(i)[arrive];
       sumOfPrevTimes += processList.get(i)[burst];
@@ -142,76 +145,14 @@ public class CpuSchedulingAlgorithmsApp{
 
     // calc averages
     double avgWaitTime = (double)totalWaitTime / n; 
+    double avgResponseTime = avgWaitTime;
     double avgTurnaroundTime = (double)totalTurnaroundTime / n;
+    double cpuUtilRate = (double)totalBurstTime / timer * 100;
 
-    System.out.println("avg wait time: " +  avgWaitTime + ", avg turnaround time: " + avgTurnaroundTime);
-  }
-
-  /**
-   * Displays the progression of the pps algorithm
-   * @param processList a list of the processes and their attributes
-   */
-  private static void printPPSProgress(ArrayList<int[]> processList){
-    int n = processList.size();
-    int[] burstTimes = new int[n];
-    for (int i = 0; i < n; i++){
-      burstTimes[i] = processList.get(i)[burst];
-    }
-
-    int count = 0;
-    int timer = 0;
-    int currentPriority = Integer.MAX_VALUE;
-    int currentProcess = 0;
-    int prevProcess = -1;
-    while (count != n){
-      // display current progress
-      if (prevProcess != currentProcess){
-        System.out.println();
-      }
-      int burstTimeFormatted = processList.get(currentProcess)[burst] - burstTimes[currentProcess];
-      System.out.print("\rProcess " + processList.get(currentProcess)[pid] + " progress: " + burstTimeFormatted + "/" + processList.get(currentProcess)[burst]);
-      prevProcess = currentProcess;
-
-      // check for newer processes 
-      for (int i = 0; i < n; i++){
-        // if a new process has arrived
-        if (timer == processList.get(i)[arrive]){
-          // and the process has a higher priority
-          if (processList.get(i)[priority] < currentPriority){
-            // then pause the current process and run the higher priority one
-            currentProcess = i;
-            currentPriority = processList.get(i)[priority];
-          }
-        }
-      }
-
-      // if current process is done, run the highest priority process that has arrived
-      if (burstTimes[currentProcess] == 0){
-        System.out.println("\nProcess " + processList.get(currentProcess)[pid] + " has finished.");
-        count++;  // denote that a process is finished
-        currentPriority = Integer.MAX_VALUE;  // reset lowest priority
-        // search through the processes
-        for (int i = 0; i < n; i++){
-          // if process has arrived and has the best priority and is not finished,  set it as the current process
-          if (processList.get(i)[arrive] < timer && processList.get(i)[priority] < currentPriority && burstTimes[i] != 0){
-            currentProcess = i;
-            currentPriority = processList.get(i)[priority];
-          }
-        }
-        // if no process fits, just increment timer
-      }
-
-      // run the process
-      try{
-        // increment timer and decrement burst timer
-        burstTimes[currentProcess]--;
-        timer++;
-        Thread.sleep(500);  // visually see progress
-      }
-      catch (InterruptedException e){
-        e.printStackTrace();
-      }
-    }
+    System.out.printf("avg wait time: %.2f\n", avgWaitTime);
+    System.out.printf("avg response time: %.2f\n", avgResponseTime);
+    System.out.printf("avg turnaround time: %.2f\n", avgTurnaroundTime);
+    System.out.printf("cpu utlization rate: %.2f%%\n", cpuUtilRate);
   }
 
   /**
@@ -231,7 +172,7 @@ public class CpuSchedulingAlgorithmsApp{
     printList(processList);
 
     // run the processes
-    runProcesses(processList);
+    runContiguousProcesses(processList);
   }
 
   /**
@@ -239,7 +180,7 @@ public class CpuSchedulingAlgorithmsApp{
    * @param processList the list of processes and their attributes
    */
   public static void shortestJobFirst(ArrayList<int[]> processList){
-    // establish process order
+    // sort by job length 
     Collections.sort(processList, new Comparator<int[]>(){
       public int compare(int[] a, int[] b){
         return Integer.compare(a[2], b[2]); // int[2] is the job length
@@ -251,7 +192,7 @@ public class CpuSchedulingAlgorithmsApp{
     printList(processList);
 
     // run processes
-    runProcesses(processList);
+    runContiguousProcesses(processList);
   }
 
   /**
@@ -259,6 +200,127 @@ public class CpuSchedulingAlgorithmsApp{
    * @param processList the list of processes and their attributes
    */
   public static void preemptivePriorityScheduling(ArrayList<int[]> processList){
+    int n = processList.size();
+    boolean[] hasStartedAlready = new boolean[n];
+    int[] startTimes = new int[n];
+    int[] turnaroundTimes = new int[n];
+    int[] burstTimes = new int[n];
+    for (int i = 0; i < n; i++){
+      burstTimes[i] = processList.get(i)[burst];
+    }
+
+    int count = 0;
+    int timer = 0;
+    int currentPriority = Integer.MAX_VALUE;
+    int currentProcess = 0;
+    int prevProcess = -1;
+    while (true){
+      // formatting newline
+      if (prevProcess != currentProcess){
+        System.out.println();
+      }
+
+      // display current progress
+      int burstTimeFormatted = processList.get(currentProcess)[burst] - burstTimes[currentProcess];
+      System.out.print("\rProcess " + processList.get(currentProcess)[pid] + " progress: " + burstTimeFormatted + "/" + processList.get(currentProcess)[burst]);
+      prevProcess = currentProcess;
+
+      // check for newer processes 
+      for (int i = 0; i < n; i++){
+        // if a new process has arrived
+        if (timer == processList.get(i)[arrive]){
+          // and the process has a higher priority
+          if (processList.get(i)[priority] < currentPriority){
+            // then pause the current process and run the higher priority one
+            currentProcess = i;
+            currentPriority = processList.get(i)[priority];
+            startTimes[i] = timer;
+            hasStartedAlready[i] = true;
+          }
+        }
+      }
+
+      // if current process is done, run the highest priority process that has already arrived
+      if (burstTimes[currentProcess] == 0){
+        // update turnaround time for finished process
+        turnaroundTimes[currentProcess] = timer - processList.get(currentProcess)[arrive];
+        count++;                              // denote that a process is finished
+        currentPriority = Integer.MAX_VALUE;  // reset priority
+        System.out.println("\nProcess " + processList.get(currentProcess)[pid] + " has finished.");
+
+        // exit if every process has concluded
+        if (count == n){
+          break;
+        }
+        else{
+          // search through the processes
+          for (int i = 0; i < n; i++){
+            // if process has arrived and has the best priority and is not finished, set it as the current process
+            if (processList.get(i)[arrive] < timer && processList.get(i)[priority] < currentPriority && burstTimes[i] != 0){
+              currentProcess = i;
+              currentPriority = processList.get(i)[priority];
+              if (hasStartedAlready[i] == false){
+                startTimes[i] = timer;
+                hasStartedAlready[i] = true;
+              }
+            }
+          }
+        }
+      }
+
+      // run the process
+      try{
+        // increment timer and decrement burst timer
+        burstTimes[currentProcess]--;
+        timer++;
+        Thread.sleep(500);  // visually see progress
+      }
+      catch (InterruptedException e){
+        e.printStackTrace();
+      }
+    }
+
+    // calc averages
+    int waitTime = 0;
+    int responseTime = 0;
+    int turnaroundTime = 0;
+    int burstTime = 0;
+    for (int i = 0; i < n; i++){
+      turnaroundTime += turnaroundTimes[i];
+      waitTime += turnaroundTimes[i] - processList.get(i)[burst];
+      responseTime += startTimes[i] - processList.get(i)[arrive];
+      burstTime += processList.get(i)[burst];
+    }
+
+    double avgWaitTime = (double)waitTime / n;
+    double avgResponseTime = (double)responseTime / n;
+    double avgTurnaroundTime = (double)turnaroundTime / n;
+    double cpuUtilRate = (double)burstTime / timer * 100;
+
+    // output stats
+    System.out.printf("avg wait time: %.2f\n", avgWaitTime);
+    System.out.printf("avg response time: %.2f\n", avgResponseTime);
+    System.out.printf("avg turnaround time: %.2f\n", avgTurnaroundTime);
+    System.out.printf("cpu utlization rate: %.2f%%\n", cpuUtilRate);
+  }
+
+  /**
+   * Implementation of the round robin scheduling algorithm
+   * @param processList the list of processes and their attributes
+   * @param q           the time quantum for each job
+   */
+  public static void roundRobin(ArrayList<int[]> processList, int q){
+    int quantum = q;
+    int n = processList.size();
+    boolean[] hasStartedAlready = new boolean[n];
+    int[] startTimes = new int[n];
+    int[] waitTimes = new int[n];
+    int[] turnaroundTimes = new int[n];
+    int[] burstTimes = new int[n];
+    for (int i = 0; i < n; i++){
+      burstTimes[i] = processList.get(i)[burst];
+    }
+
     // sort by arrival time
     Collections.sort(processList, new Comparator<int[]>(){
       public int compare(int[] a, int[] b){
@@ -266,16 +328,88 @@ public class CpuSchedulingAlgorithmsApp{
       }
     });
 
-    // print progress
-    printPPSProgress(processList);
-  }
+    // stash each process into a queue
+    // if the quantum is over but the next one in queue hasn't arrived yet, wait a quantum
+    Queue<int[]> processQueue = new LinkedList<>();
+    for (int i = 0; i < n; i++){
+      processQueue.add(processList.get(i));
+    }
+    int timer = 0;
+    while (true){
+      // display current progress
+      int currentPid = processQueue.peek()[pid];
+      int burstTimeFormatted = burstTimes[currentPid-1] - processQueue.peek()[burst];
+      System.out.print("\rProcess " + currentPid  + " progress: " + burstTimeFormatted + "/" + burstTimes[currentPid-1]);
 
-  /**
-   * Implementation of the round robin scheduling algorithm
-   * @param processList the list of processes and their attributes
-   * @param quantum     the time quantum for each job
-   */
-  public static void roundRobin(ArrayList<int[]> processList, int quantum){
 
+      // if a current process is done or quantum is up
+      if (processQueue.peek()[burst] == 0 || quantum == 0){
+        System.out.println();
+        // if the process is done, pop it
+        if (processQueue.peek()[burst] == 0){
+          turnaroundTimes[currentPid-1] = timer - processQueue.peek()[arrive];
+          System.out.println("\nProcess " + processQueue.peek()[pid] + " has finished.");
+          processQueue.remove();
+
+          // exit if every process has concluded
+          if (processQueue.peek() == null){
+            break;
+          }
+        }
+        // if only the quantum is over and not the process, move it to the end 
+        else{
+          processQueue.add(processQueue.remove());
+        }
+        quantum = q;  // reset quantum
+      }
+
+      // run the process
+      try{
+        // if the process is ready, run it
+        if (processQueue.peek()[arrive] <= timer){
+          // start collecting stats for process start times
+          if (hasStartedAlready[currentPid-1] == false){
+            startTimes[currentPid-1] = timer;
+            hasStartedAlready[currentPid-1] = true;
+          }
+          // increment timer, decrement burst, decrement quantum
+          processQueue.peek()[burst]--;
+          timer++;
+          quantum--;
+        }
+        // else run the timer for the quantum
+        else{
+          quantum -= q;
+          timer += q;
+        }
+        Thread.sleep(500);
+      }
+      catch (InterruptedException e){
+        e.printStackTrace();
+      }
+    }
+
+    // calc stats
+    int waitTime = 0;
+    int responseTime = 0;
+    int turnaroundTime = 0;
+    int burstTime = 0;
+    for (int i = 0; i < n; i++){
+      waitTime += turnaroundTimes[i] - burstTimes[i];
+      responseTime += startTimes[i] - processList.get(i)[arrive];
+      turnaroundTime += turnaroundTimes[i];
+      burstTime += burstTimes[i];
+    }
+
+    double avgWaitTime = (double)waitTime / n;
+    double avgResponseTime = (double)responseTime / n;
+    double avgTurnaroundTime = (double)turnaroundTime / n;
+    double cpuUtilRate = (double)burstTime / timer * 100;
+
+    // output stats
+    System.out.printf("avg wait time: %.2f\n", avgWaitTime);
+    System.out.printf("avg response time: %.2f\n", avgResponseTime);
+    System.out.printf("avg turnaround time: %.2f\n", avgTurnaroundTime);
+    System.out.printf("cpu utlization rate: %.2f%%\n", cpuUtilRate);
   }
 }
